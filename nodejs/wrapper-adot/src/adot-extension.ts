0cdf3b0e79eac3eab23a326fc4f5db1df16d7990
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-04-20 16:31:10 +0900

    Customize adot nodejs layer with xray id generator

---
+++ nodejs/wrapper-adot/src/adot-extension.ts
@@ -0,0 +1,13 @@
+import {NodeTracerConfig} from "@opentelemetry/node";
+import {AWSXRayIdGenerator} from "@opentelemetry/id-generator-aws-xray";
+
+declare global {
+    function configureTracer(defaultConfig: NodeTracerConfig): NodeTracerConfig;
+}
+
+global.configureTracer = (config: NodeTracerConfig) => {
+    return {
+        ...config,
+        idGenerator: new AWSXRayIdGenerator(),
+    };
+}
