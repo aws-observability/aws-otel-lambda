5420469ac05b530f77bb9e2dc6b5f76e59ef85e7
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-04-21 17:22:42 +0900

    Wrap integration tests to enable xray

---
+++ integration-tests/java/aws-sdk/agent/outputs.tf
@@ -0,0 +1,3 @@
+output "api-gateway-url" {
+  value = module.test.api-gateway-url
+}
--- nodejs/build.sh
+++ nodejs/build.sh
@@ -13,7 +13,6 @@ cp "$SOURCEDIR"/scripts/otel-handler ./packages/layer/build/workspace/otel-handl
 # This assumes that id-generator-aws-xray does not have additional dependencies outside of normal OTel ones.
 # It's inconceivable for this to ever be incorrect.
 cp -r "$SOURCEDIR"/wrapper-adot/node_modules/@opentelemetry/id-generator-aws-xray ./packages/layer/build/workspace/nodejs/node_modules/@opentelemetry
-ls "$SOURCEDIR"/wrapper-adot/build/src/
 cp "$SOURCEDIR"/wrapper-adot/build/src/adot-extension.* ./packages/layer/build/workspace/
 
 cd ./packages/layer/build/workspace || exit
