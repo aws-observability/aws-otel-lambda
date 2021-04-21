633507015fc11176527cebff3c1474907c4e04dc
Anuraag Agrawal <anuraaga@gmail.com>
2021-04-21 09:35:36 +0900

    Customize adot nodejs layer with xray id generator (#35)
    * Customize adot nodejs layer with xray id generator
    
    * Fix directory for npm install
    
    * lint
    
    * quieter zip

---
+++ nodejs/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SOURCEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd wrapper-adot || exit
+npm install
+
+cd ../../opentelemetry-lambda/nodejs || exit
+npm install
+
+mv ./packages/layer/build/workspace/otel-handler ./packages/layer/build/workspace/otel-handler-upstream
+cp "$SOURCEDIR"/scripts/otel-handler ./packages/layer/build/workspace/otel-handler
+# This assumes that id-generator-aws-xray does not have additional dependencies outside of normal OTel ones.
+# It's inconceivable for this to ever be incorrect.
+cp -r "$SOURCEDIR"/wrapper-adot/node_modules/@opentelemetry/id-generator-aws-xray ./packages/layer/build/workspace/nodejs/node_modules/@opentelemetry
+ls "$SOURCEDIR"/wrapper-adot/build/src/
+cp "$SOURCEDIR"/wrapper-adot/build/src/adot-extension.* ./packages/layer/build/workspace/
+
+cd ./packages/layer/build/workspace || exit
+rm ../layer.zip
+zip -qr ../layer.zip *
