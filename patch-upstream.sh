b368b8ba6b6588cc7c03e24461f55b6e6c0199ee
Wang <wangzl@amazon.com>
2021-04-21 23:27:59 -0700

    bump aoc collector to v0.9.0

--- patch-upstream.sh
+++ patch-upstream.sh
@@ -1,7 +1,8 @@
 cp -rf adot/* opentelemetry-lambda/
 cd opentelemetry-lambda/collector
-# temporary unblock lambdaComponent patch, will update to aws-otel-lambda/lambdaComponent soon
-# go mod edit -replace github.com/open-telemetry/opentelemetry-lambda/collector/lambdacomponents=github.com/aws-observability/aws-otel-collector/pkg/lambdacomponents@0.8.2
-go mod edit -replace github.com/open-telemetry/opentelemetry-collector-contrib/internal/aws=github.com/open-telemetry/opentelemetry-collector-contrib/internal/aws@v0.23.0
-go mod edit -replace github.com/open-telemetry/opentelemetry-collector-contrib/internal/awsxray=github.com/open-telemetry/opentelemetry-collector-contrib/internal/awsxray@v0.23.0
+# replace collector with AOC
+go mod edit -replace github.com/open-telemetry/opentelemetry-lambda/collector/lambdacomponents=github.com/aws-observability/aws-otel-collector/pkg/lambdacomponents@v0.9.0
+go mod edit -replace github.com/open-telemetry/opentelemetry-collector-contrib/internal/aws/awsutil=github.com/open-telemetry/opentelemetry-collector-contrib/internal/aws/awsutil@v0.25.0
+go mod edit -replace github.com/open-telemetry/opentelemetry-collector-contrib/internal/aws/metrics=github.com/open-telemetry/opentelemetry-collector-contrib/internal/aws/metrics@v0.25.0
+go mod edit -replace github.com/open-telemetry/opentelemetry-collector-contrib/internal/aws/xray=github.com/open-telemetry/opentelemetry-collector-contrib/internal/aws/xray@v0.25.0
 go mod tidy
