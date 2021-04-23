b7d32cc71716f8de6786a2c78d85f15c9c554a0d
Anuraag Agrawal <anuraaga@gmail.com>
2021-04-23 14:46:13 +0900

    Update sample app / canary to combined layers (#43)

--- sample-apps/terraform/nodejs/aws-sdk/main.tf
+++ sample-apps/terraform/nodejs/aws-sdk/main.tf
@@ -2,17 +2,14 @@ data "aws_region" "current" {}
 
 # TODO(anuraaga): Replace with public layers, these currently only work with authenticated requests for testing.
 locals {
-  collector_layer_arns = {
-    "us-east-1" = "arn:aws:lambda:us-east-1:611364707713:layer:aws-otel-lambda-collector-93d3e85f71b82de431916d03c96f221812d9841b:4"
-  }
   sdk_layer_arns = {
-    "us-east-1" = "arn:aws:lambda:us-east-1:611364707713:layer:aws-otel-lambda-nodejs-93d3e85f71b82de431916d03c96f221812d9841b:1"
+    "us-east-1" = "arn:aws:lambda:us-east-1:611364707713:layer:aws-otel-lambda-nodejs-e6c1e7530c1d4fcce4ac3af5b354355edcbc7769:1"
   }
 }
 
 module "app" {
   source = "../../../../opentelemetry-lambda/nodejs/sample-apps/aws-sdk/deploy"
 
-  collector_layer_arn      = lookup(local.collector_layer_arns, data.aws_region.current.name, "invalid")
+  collector_layer_arn      = null
   nodejs_wrapper_layer_arn = lookup(local.sdk_layer_arns, data.aws_region.current.name, "invalid")
 }
