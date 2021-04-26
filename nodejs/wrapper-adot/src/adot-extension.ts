fabce75a6e4033928e8bf75eff049fc2f33c9bd9
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-04-26 16:00:23 +0900

    Enable default propagators in nodejs if not set.

--- nodejs/wrapper-adot/src/adot-extension.ts
+++ nodejs/wrapper-adot/src/adot-extension.ts
@@ -1,8 +1,28 @@
+import { propagation } from '@opentelemetry/api';
+import { CompositePropagator, HttpTraceContext } from '@opentelemetry/core';
 import { NodeTracerConfig } from '@opentelemetry/node';
+import {
+  B3SinglePropagator,
+  B3MultiPropagator,
+} from '@opentelemetry/propagator-b3';
 import { AWSXRayIdGenerator } from '@opentelemetry/id-generator-aws-xray';
+import { AWSXRayPropagator } from '@opentelemetry/propagator-aws-xray';
 
 declare global {
   function configureTracer(defaultConfig: NodeTracerConfig): NodeTracerConfig;
+}
+
+if (!process.env.OTEL_PROPAGATORS) {
+  propagation.setGlobalPropagator(
+    new CompositePropagator({
+      propagators: [
+        new AWSXRayPropagator(),
+        new HttpTraceContext(),
+        new B3SinglePropagator(),
+        new B3MultiPropagator(),
+      ],
+    })
+  );
 }
 
 global.configureTracer = (config: NodeTracerConfig) => {
