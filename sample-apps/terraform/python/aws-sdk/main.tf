c21acc8aab7bc428d51753d0303581a04c27f0ee
Lei Wang <66336933+wangzlei@users.noreply.github.com>
2021-04-26 20:58:20 -0700

    Enable python canary (#56)
    * update soak workflow
    
    * tweak soak config for different layer kind
    
    * update soak config for java layers
    
    * tweak soak config for python
    
    * enable python canary

--- sample-apps/terraform/python/aws-sdk/main.tf
+++ sample-apps/terraform/python/aws-sdk/main.tf
@@ -3,7 +3,23 @@ data "aws_region" "current" {}
 # TODO(anuraaga): Replace with public layers, these currently only work with authenticated requests for testing.
 locals {
   sdk_layer_arns = {
-    "us-east-1" = "arn:aws:lambda:us-east-1:611364707713:layer:aws-otel-lambda-python-e6c1e7530c1d4fcce4ac3af5b354355edcbc7769:1"
+    "ap-northeast-1" = "arn:aws:lambda:ap-northeast-1:901920570463:layer:aws-otel_python38_ver-0-0-0:1"
+    "ap-northeast-2" = "arn:aws:lambda:ap-northeast-2:901920570463:layer:aws-otel_python38_ver-0-0-0:1"
+    "ap-northeast-3" = "arn:aws:lambda:ap-northeast-3:901920570463:layer:aws-otel_python38_ver-0-0-0:1"
+    "ap-south-1" = "arn:aws:lambda:ap-south-1:901920570463:layer:aws-otel_python38_ver-0-0-0:1"
+    "ap-southeast-1" = "arn:aws:lambda:ap-southeast-1:901920570463:layer:aws-otel_python38_ver-0-0-0:1"
+    "ap-southeast-2" = "arn:aws:lambda:ap-southeast-2:901920570463:layer:aws-otel_python38_ver-0-0-0:1"
+    "ca-central-1" = "arn:aws:lambda:ca-central-1:901920570463:layer:aws-otel_python38_ver-0-0-0:1"
+    "eu-central-1" = "arn:aws:lambda:eu-central-1:901920570463:layer:aws-otel_python38_ver-0-0-0:1"
+    "eu-north-1" = "arn:aws:lambda:eu-north-1:901920570463:layer:aws-otel_python38_ver-0-0-0:1"
+    "eu-west-1" = "arn:aws:lambda:eu-west-1:901920570463:layer:aws-otel_python38_ver-0-0-0:1"
+    "eu-west-2" = "arn:aws:lambda:eu-west-2:901920570463:layer:aws-otel_python38_ver-0-0-0:1"
+    "eu-west-3" = "arn:aws:lambda:eu-west-3:901920570463:layer:aws-otel_python38_ver-0-0-0:1"
+    "sa-east-1" = "arn:aws:lambda:sa-east-1:901920570463:layer:aws-otel_python38_ver-0-0-0:1"
+    "us-east-1" = "arn:aws:lambda:us-east-1:901920570463:layer:aws-otel_python38_ver-0-0-0:1"
+    "us-east-2" = "arn:aws:lambda:us-east-2:901920570463:layer:aws-otel_python38_ver-0-0-0:1"
+    "us-west-1" = "arn:aws:lambda:us-west-1:901920570463:layer:aws-otel_python38_ver-0-0-0:1"
+    "us-west-2" = "arn:aws:lambda:us-west-2:901920570463:layer:aws-otel_python38_ver-0-0-0:1"
   }
 }
 
