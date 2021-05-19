f97ddca425ddef0bfcf94c7b6eaf58225e12135f
Wang <wangzl@amazon.com>
2021-05-18 21:55:31 -0700

    add smoke test in release workflow

---
+++ sample-apps/python-aws-sdk-aiohttp-terraform/layer.tf
@@ -0,0 +1,20 @@
+locals {
+  sdk_layer_arns = {
+    "ap-northeast-1" = "arn:aws:lambda:ap-northeast-1:901920570463:layer:aws-otel-python38-ver-1-1-0:1"
+    "ap-northeast-2" = "arn:aws:lambda:ap-northeast-2:901920570463:layer:aws-otel-python38-ver-1-1-0:1"
+    "ap-south-1" = "arn:aws:lambda:ap-south-1:901920570463:layer:aws-otel-python38-ver-1-1-0:1"
+    "ap-southeast-1" = "arn:aws:lambda:ap-southeast-1:901920570463:layer:aws-otel-python38-ver-1-1-0:1"
+    "ap-southeast-2" = "arn:aws:lambda:ap-southeast-2:901920570463:layer:aws-otel-python38-ver-1-1-0:1"
+    "ca-central-1" = "arn:aws:lambda:ca-central-1:901920570463:layer:aws-otel-python38-ver-1-1-0:1"
+    "eu-central-1" = "arn:aws:lambda:eu-central-1:901920570463:layer:aws-otel-python38-ver-1-1-0:1"
+    "eu-north-1" = "arn:aws:lambda:eu-north-1:901920570463:layer:aws-otel-python38-ver-1-1-0:1"
+    "eu-west-1" = "arn:aws:lambda:eu-west-1:901920570463:layer:aws-otel-python38-ver-1-1-0:1"
+    "eu-west-2" = "arn:aws:lambda:eu-west-2:901920570463:layer:aws-otel-python38-ver-1-1-0:1"
+    "eu-west-3" = "arn:aws:lambda:eu-west-3:901920570463:layer:aws-otel-python38-ver-1-1-0:1"
+    "sa-east-1" = "arn:aws:lambda:sa-east-1:901920570463:layer:aws-otel-python38-ver-1-1-0:1"
+    "us-east-1" = "arn:aws:lambda:us-east-1:901920570463:layer:aws-otel-python38-ver-1-1-0:1"
+    "us-east-2" = "arn:aws:lambda:us-east-2:901920570463:layer:aws-otel-python38-ver-1-1-0:1"
+    "us-west-1" = "arn:aws:lambda:us-west-1:901920570463:layer:aws-otel-python38-ver-1-1-0:1"
+    "us-west-2" = "arn:aws:lambda:us-west-2:901920570463:layer:aws-otel-python38-ver-1-1-0:1"
+  }
+}
