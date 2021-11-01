2d7ce2cc290ac066975b343f4dfc9b067e05a1bb
(Eliseo) Nathaniel Ruiz Nowell <nathanielruiz98@gmail.com>
2021-11-01 16:10:20 -0400

    Python auto-instr script calls upstream script

--- python/build.sh
+++ python/build.sh
@@ -20,4 +20,11 @@ cd ./build || exit
 unzip -qo layer.zip
 rm layer.zip
 unzip -qo ../../../collector/build/collector-extension.zip
+
+# Use our AWS scripts instead which extend and call OTel Lambda scripts
+mv otel-instrument otel-instrument-upstream-lambda
+cp ../../../../python/scripts/otel-instrument .
+
+# Zip all the files in `opentelemetry-lambda/python/src/build` together for the
+# layer
 zip -qr layer.zip *
