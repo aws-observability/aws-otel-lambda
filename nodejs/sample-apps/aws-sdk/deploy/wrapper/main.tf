385ae869de81cab5ca4a2e81f0f9cda9df36c32e
(Eliseo) Nathaniel Ruiz Nowell <nathanielruiz98@gmail.com>
2021-11-26 19:23:37 -0800

    Make testing file paths consistent
    * Put all testing files under <language>/<app_type>/<instrumentation_type> directory for consistency

---
+++ nodejs/sample-apps/aws-sdk/deploy/wrapper/main.tf
@@ -0,0 +1,15 @@
+data "aws_region" "current" {}
+
+module "app" {
+  source = "../../../../../opentelemetry-lambda/nodejs/sample-apps/aws-sdk/deploy/wrapper"
+
+  name                = var.function_name
+  collector_layer_arn = null
+  sdk_layer_arn       = lookup(local.sdk_layer_arns, data.aws_region.current.name, "invalid")
+  tracing_mode        = "Active"
+}
+
+resource "aws_iam_role_policy_attachment" "test_xray" {
+  role       = module.app.function_role_name
+  policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
+}
