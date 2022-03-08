1e9efb23a982963c84ad6280dccdf524eecb7f78
Pavan Sai Vasireddy (Vasi) <pvasir@amazon.com>
2022-03-07 16:15:45 -0800

    update layer arn's

--- nodejs/sample-apps/aws-sdk/deploy/wrapper/layer_arm64.tf
+++ nodejs/sample-apps/aws-sdk/deploy/wrapper/layer_arm64.tf
@@ -1,4 +1,14 @@
 locals {
   sdk_layer_arns_arm64 = {
+    "ap-northeast-1" = "arn:aws:lambda:ap-northeast-1:901920570463:layer:aws-otel-nodejs-arm64-ver-1-0-1:2"
+    "ap-south-1"     = "arn:aws:lambda:ap-south-1:901920570463:layer:aws-otel-nodejs-arm64-ver-1-0-1:2"
+    "ap-southeast-1" = "arn:aws:lambda:ap-southeast-1:901920570463:layer:aws-otel-nodejs-arm64-ver-1-0-1:2"
+    "ap-southeast-2" = "arn:aws:lambda:ap-southeast-2:901920570463:layer:aws-otel-nodejs-arm64-ver-1-0-1:2"
+    "eu-central-1"   = "arn:aws:lambda:eu-central-1:901920570463:layer:aws-otel-nodejs-arm64-ver-1-0-1:2"
+    "eu-west-1"      = "arn:aws:lambda:eu-west-1:901920570463:layer:aws-otel-nodejs-arm64-ver-1-0-1:2"
+    "eu-west-2"      = "arn:aws:lambda:eu-west-2:901920570463:layer:aws-otel-nodejs-arm64-ver-1-0-1:2"
+    "us-east-1"      = "arn:aws:lambda:us-east-1:901920570463:layer:aws-otel-nodejs-arm64-ver-1-0-1:2"
+    "us-east-2"      = "arn:aws:lambda:us-east-2:901920570463:layer:aws-otel-nodejs-arm64-ver-1-0-1:2"
+    "us-west-2"      = "arn:aws:lambda:us-west-2:901920570463:layer:aws-otel-nodejs-arm64-ver-1-0-1:2"
   }
 }
