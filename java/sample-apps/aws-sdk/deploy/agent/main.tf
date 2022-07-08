07a09789c8a7fbb2236df39ca59c4ae680a34a6f
Vasi Vasireddy <41936996+vasireddy99@users.noreply.github.com>
2022-07-08 12:12:20 -0700

    Use Sigv4 Authentication for Prometheus Remote Write exporter (#258)
    * Use Sigv4 Authentication for Prometheus Remote Write exporter
    
    * Draft for Changelog
    
    * set region
    
    * revert the readme

--- java/sample-apps/aws-sdk/deploy/agent/main.tf
+++ java/sample-apps/aws-sdk/deploy/agent/main.tf
@@ -38,6 +38,10 @@ data "archive_file" "init" {
   depends_on = [aws_prometheus_workspace.test_amp_workspace[0], data.aws_region.current]
   source {
     content  = <<EOT
+extensions:
+  sigv4auth:
+    region: ${data.aws_region.current.name}
+
 receivers:
   otlp:
     protocols:
@@ -47,20 +51,20 @@ receivers:
 exporters:
   logging:
   awsxray:
-  awsprometheusremotewrite:
+  prometheusremotewrite:
     endpoint: "${aws_prometheus_workspace.test_amp_workspace[0].prometheus_endpoint}api/v1/remote_write"
-    aws_auth:
-      service: "aps"
-      region: "${data.aws_region.current.name}"
+    auth:
+      authenticator: sigv4auth
 
 service:
+  extensions: [sigv4auth]
   pipelines:
     traces:
       receivers: [otlp]
       exporters: [awsxray]
     metrics:
       receivers: [otlp]
-      exporters: [logging, awsprometheusremotewrite]
+      exporters: [logging, prometheusremotewrite]
 EOT
     filename = "config.yaml"
   }
