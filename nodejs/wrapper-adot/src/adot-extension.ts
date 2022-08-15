40a949d2d4629d2338942aceabc54b4ca1525425
Anthony Mirabella <a9@aneurysm9.com>
2022-08-15 19:34:38 -0400

    Fix global propagator registration (#280)
    * Fix global propagator registration
    
    Avoid duplicate registration errors by using the
    `global.configureSdkRegistration()` callback to set
    the `propagator` field of the `SDKRegistrationConfig`
    rather than explicitly calling `propagation.setGlobalPropagator()`.
    
    Signed-off-by: Anthony J Mirabella <a9@aneurysm9.com>
    
    * update build.sh to set -x for debugging
    
    Signed-off-by: Anthony J Mirabella <a9@aneurysm9.com>
    
    * Make @opentelemetry/api a dev dependency
    
    Signed-off-by: Anthony J Mirabella <a9@aneurysm9.com>
    
    Signed-off-by: Anthony J Mirabella <a9@aneurysm9.com>

--- nodejs/wrapper-adot/src/adot-extension.ts
+++ nodejs/wrapper-adot/src/adot-extension.ts
@@ -1,25 +1,29 @@
-import { propagation } from '@opentelemetry/api';
 import { CompositePropagator, W3CTraceContextPropagator } from '@opentelemetry/core';
+import { SDKRegistrationConfig } from '@opentelemetry/sdk-trace-base';
 import { NodeTracerConfig } from '@opentelemetry/sdk-trace-node';
 import { B3InjectEncoding, B3Propagator } from '@opentelemetry/propagator-b3';
 import { AWSXRayIdGenerator } from '@opentelemetry/id-generator-aws-xray';
 import { AWSXRayPropagator } from '@opentelemetry/propagator-aws-xray';
 
 declare global {
+  function configureSdkRegistration(defaultSdkRegistration: SDKRegistrationConfig): SDKRegistrationConfig;
   function configureTracer(defaultConfig: NodeTracerConfig): NodeTracerConfig;
 }
 
 if (!process.env.OTEL_PROPAGATORS) {
-  propagation.setGlobalPropagator(
-    new CompositePropagator({
-      propagators: [
-        new AWSXRayPropagator(),
-        new W3CTraceContextPropagator(),
-        new B3Propagator(),
-        new B3Propagator({ injectEncoding: B3InjectEncoding.MULTI_HEADER }),
-      ],
-    })
-  );
+  global.configureSdkRegistration = (config: SDKRegistrationConfig) => {
+      return{
+        ...config,
+        propagator: new CompositePropagator({
+          propagators: [
+            new AWSXRayPropagator(),
+            new W3CTraceContextPropagator(),
+            new B3Propagator(),
+            new B3Propagator({ injectEncoding: B3InjectEncoding.MULTI_HEADER }),
+          ],
+        }),
+      };
+  }
 }
 
 global.configureTracer = (config: NodeTracerConfig) => {
