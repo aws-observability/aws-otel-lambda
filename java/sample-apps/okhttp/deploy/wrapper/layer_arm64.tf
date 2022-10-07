c072ca5e7231f75c0f6d5c7381ed1a6d106866fe
Pavan Sai Vasireddy (Vasi) <pvasir@amazon.com>
2022-10-06 17:03:01 -0700

    Lambda Layer Update: Layer ARNs to the latest published Oct-2022

--- java/sample-apps/okhttp/deploy/wrapper/layer_arm64.tf
+++ java/sample-apps/okhttp/deploy/wrapper/layer_arm64.tf
@@ -1,14 +1,14 @@
 locals {
   sdk_layer_arns_arm64 = {
-    "ap-northeast-1" = "arn:aws:lambda:ap-northeast-1:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-17-0:1"
-    "ap-south-1"     = "arn:aws:lambda:ap-south-1:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-17-0:1"
-    "ap-southeast-1" = "arn:aws:lambda:ap-southeast-1:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-17-0:1"
-    "ap-southeast-2" = "arn:aws:lambda:ap-southeast-2:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-17-0:1"
-    "eu-central-1"   = "arn:aws:lambda:eu-central-1:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-17-0:1"
-    "eu-west-1"      = "arn:aws:lambda:eu-west-1:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-17-0:1"
-    "eu-west-2"      = "arn:aws:lambda:eu-west-2:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-17-0:1"
-    "us-east-1"      = "arn:aws:lambda:us-east-1:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-17-0:1"
-    "us-east-2"      = "arn:aws:lambda:us-east-2:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-17-0:1"
-    "us-west-2"      = "arn:aws:lambda:us-west-2:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-17-0:1"
+    "ap-northeast-1" = "arn:aws:lambda:ap-northeast-1:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-18-0:1"
+    "ap-south-1"     = "arn:aws:lambda:ap-south-1:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-18-0:1"
+    "ap-southeast-1" = "arn:aws:lambda:ap-southeast-1:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-18-0:1"
+    "ap-southeast-2" = "arn:aws:lambda:ap-southeast-2:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-18-0:1"
+    "eu-central-1"   = "arn:aws:lambda:eu-central-1:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-18-0:1"
+    "eu-west-1"      = "arn:aws:lambda:eu-west-1:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-18-0:1"
+    "eu-west-2"      = "arn:aws:lambda:eu-west-2:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-18-0:1"
+    "us-east-1"      = "arn:aws:lambda:us-east-1:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-18-0:1"
+    "us-east-2"      = "arn:aws:lambda:us-east-2:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-18-0:1"
+    "us-west-2"      = "arn:aws:lambda:us-west-2:901920570463:layer:aws-otel-java-wrapper-arm64-ver-1-18-0:1"
   }
 }
