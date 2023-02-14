92b36c6bd8421706a80e86cf8804d21c11c4bb71
kausik-A <ucangetkausik@gmail.com>
2023-02-14 10:31:17 -0800

    Edited patch file patch

--- patch-upstream.sh
+++ patch-upstream.sh
@@ -19,10 +19,10 @@ cd opentelemetry-lambda/collector
 
 # patch collector startup to remove HTTP and S3 confmap providers
 # and set ADOT-specific BuildInfo
-patch < ../../collector.patch
+patch -p2 < ../../collector.patch
 
 # patch manager.go to remove lambdacomponents attribute
-patch < ../../manager.patch
+patch -p2 < ../../manager.patch
 
 # Replace OTel Collector with ADOT Collector
 go mod edit -replace github.com/open-telemetry/opentelemetry-lambda/collector/lambdacomponents=github.com/aws-observability/aws-otel-collector/pkg/lambdacomponents@v0.26.0
