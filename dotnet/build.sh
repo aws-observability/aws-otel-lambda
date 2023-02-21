01ec8fd89d9b72912166fc9553a85c584161b19e
kausik-A <ucangetkausik@gmail.com>
2023-02-20 17:08:53 -0800

    Introduce Global.json

--- dotnet/build.sh
+++ dotnet/build.sh
@@ -6,6 +6,9 @@ pushd ../opentelemetry-lambda/collector || exit
 make package
 popd || exit
 
+# Specify the path to the .NET version used 
+cp global.json ../opentelemetry-lambda/dotnet/sample-apps/aws-sdk/wrapper/SampleApps
+
 # Build sample app
 
 cd ../opentelemetry-lambda/dotnet/sample-apps/aws-sdk/wrapper/SampleApps || exit
