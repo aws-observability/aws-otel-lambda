e5bd087b5055c3e1c094a762f43ce644f173a689
Pavan Sai Vasireddy (Vasi) <pvasir@amazon.com>
2023-04-21 15:22:14 -0700

    Modify env and correct tf script

--- java/integration-tests/aws-sdk/agent-confmap/main.tf
+++ java/integration-tests/aws-sdk/agent-confmap/main.tf
@@ -1,3 +1,4 @@
+data "aws_region" "current" {}
 
 locals {
   architecture = var.architecture == "x86_64" ? "amd64" : "arm64"
@@ -14,10 +15,10 @@ resource "aws_lambda_layer_version" "sdk_layer" {
 resource "aws_lambda_layer_version" "collector_layer" {
   count               = var.enable_collector_layer ? 1 : 0
   layer_name          = var.collector_layer_name
-  filename            = ".${path.module}/../../../../opentelemetry-lambda/collector/build/collector-extension-${local.architecture}.zip"
+  filename            = "${path.module}/../../../../opentelemetry-lambda/collector/build/collector-extension-${local.architecture}.zip"
   compatible_runtimes = ["nodejs14.x", "nodejs16.x", "nodejs18.x"]
   license_info        = "Apache-2.0"
-  source_code_hash    = filebase64sha256(".${path.module}/../../../../opentelemetry-lambda/collector/build/collector-extension-${local.architecture}.zip")
+  source_code_hash    = filebase64sha256("${path.module}/../../../../opentelemetry-lambda/collector/build/collector-extension-${local.architecture}.zip")
 }
 
 module "hello-lambda-function" {
@@ -26,7 +27,7 @@ module "hello-lambda-function" {
   architecture               = var.architecture
   collector_layer_arn        = var.enable_collector_layer ? aws_lambda_layer_version.collector_layer[0].arn : null
   sdk_layer_arn              = aws_lambda_layer_version.sdk_layer.arn
-  collector_config_layer_arn = length(aws_lambda_layer_version.collector_config_layer) > 0 ? aws_lambda_layer_version.collector_config_layer[0].arn : null
+  collector_config_layer_arn = aws_lambda_layer_version.collector_config_layer.arn
   tracing_mode               = var.tracing_mode
 }
 
@@ -34,9 +35,6 @@ resource "aws_iam_role_policy_attachment" "hello-lambda-cloudwatch-insights" {
   role       = module.hello-lambda-function.function_role_name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
-
-
-data "aws_region" "current" {}
 
 resource "aws_prometheus_workspace" "test_amp_workspace" {
   alias = var.function_name
