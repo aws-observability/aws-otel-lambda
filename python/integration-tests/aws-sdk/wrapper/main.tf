d508bca870e3a2f3d78287d9a5dfdf6af554ae15
Huy Vo <hvoam@amazon.com>
2023-05-31 16:19:51 -0700

    Create python sample app downstream (#578)

--- python/integration-tests/aws-sdk/wrapper/main.tf
+++ python/integration-tests/aws-sdk/wrapper/main.tf
@@ -19,22 +19,59 @@ resource "aws_lambda_layer_version" "collector_layer" {
   source_code_hash    = filebase64sha256("${path.module}/../../../../opentelemetry-lambda/collector/build/collector-extension-${local.architecture}.zip")
 }
 
-module "hello-lambda-function" {
-  source              = "../../../../opentelemetry-lambda/python/sample-apps/aws-sdk/deploy/wrapper"
+module "test-function" {
+  source  = "terraform-aws-modules/lambda/aws"
+
+  architectures = compact([var.architecture])
+  function_name = var.function_name
+  handler       = "lambda_function.lambda_handler"
+  runtime       = var.runtime
+
+  create_package         = false
+  local_existing_package = "${path.module}/../../../sample-apps/build/function.zip"
+
+  timeout     = 20
+
+  layers = compact([
+    var.enable_collector_layer ? aws_lambda_layer_version.collector_layer[0].arn : null,
+    aws_lambda_layer_version.sdk_layer.arn
+  ])
+
+  environment_variables = {
+    AWS_LAMBDA_EXEC_WRAPPER = "/opt/otel-instrument"
+  }
+
+  tracing_mode = var.tracing_mode
+
+  attach_policy_statements = true
+  policy_statements = {
+    s3 = {
+      effect = "Allow"
+      actions = [
+        "s3:ListAllMyBuckets"
+      ]
+      resources = [
+        "*"
+      ]
+    }
+  }
+}
+
+module "api-gateway" {
+  source = "../../../../opentelemetry-lambda/utils/terraform/api-gateway-proxy"
+
   name                = var.function_name
-  architecture        = var.architecture
-  collector_layer_arn = var.enable_collector_layer ? aws_lambda_layer_version.collector_layer[0].arn : null
-  sdk_layer_arn       = aws_lambda_layer_version.sdk_layer.arn
-  tracing_mode        = var.tracing_mode
-  runtime             = var.runtime
+  function_name       = module.test-function.lambda_function_name
+  function_invoke_arn = module.test-function.lambda_function_invoke_arn
+  enable_xray_tracing = true
 }
 
 resource "aws_iam_role_policy_attachment" "hello-lambda-cloudwatch-insights" {
-  role       = module.hello-lambda-function.function_role_name
+  role       = module.test-function.lambda_function_name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_iam_role_policy_attachment" "test_xray" {
-  role       = module.hello-lambda-function.function_role_name
+  role       = module.test-function.lambda_function_name
   policy_arn = "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess"
 }
