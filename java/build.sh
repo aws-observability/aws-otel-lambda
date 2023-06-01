bf1e52620fd90c42e002b971256654e198b1758c
Raphael Philipe Mendes da Silva <rapphil@gmail.com>
2023-06-01 10:27:11 -0700

    POC to fix lambda layers (#572)
    * POC for reverting lambda context propagation change
    
    * fix build.sh
    
    * Fix build.sh
    
    * Fix patch
    
    * Increase duration for creds
    
    * Remove run on pull request
    
    ---------
    
    Co-authored-by: Huy Vo <hvoam@amazon.com>

--- java/build.sh
+++ java/build.sh
@@ -2,6 +2,34 @@
 
 SOURCEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+
+## revert https://github.com/open-telemetry/opentelemetry-java-instrumentation/pull/7970
+
+git clone https://github.com/open-telemetry/opentelemetry-java-instrumentation.git
+pushd opentelemetry-java-instrumentation
+git checkout v1.26.0 -b tag-v1.26.0
+patch -p1 < "$SOURCEDIR"/../patches/opentelemetry-java-instrumentation.patch
+git add -A
+git commit -m "Create patch version"
+./gradlew publishToMavenLocal
+popd
+
+rm -rf opentelemetry-java-instrumentation
+
+
+git clone https://github.com/aws-observability/aws-otel-java-instrumentation.git
+
+pushd aws-otel-java-instrumentation
+
+git checkout v1.26.0 -b tag-v1.26.0
+patch -p1 < "${SOURCEDIR}"/../patches/aws-otel-java-instrumentation.patch
+git add -A
+git commit -a -m "Create patch version"
+CI=false ./gradlew publishToMavenLocal -Prelease.version=1.26.0-adot-lambda1
+popd
+
+rm -rf aws-otel-java-instrumentation
+
 # Build collector
 
 pushd ../opentelemetry-lambda/collector || exit
@@ -13,12 +41,12 @@ popd || exit
 ./gradlew build
 
 # Move the ADOT Lambda Java SDK code into OTel Lambda Java folder
-mkdir -p ../opentelemetry-lambda/java/build/extensions
-cp ./build/libs/aws-otel-lambda-java-extensions.jar ../opentelemetry-lambda/java/build/extensions
+mkdir -p ../opentelemetry-lambda/java/layer-wrapper/build/extensions
+cp ./build/libs/aws-otel-lambda-java-extensions.jar ../opentelemetry-lambda/java/layer-wrapper/build/extensions
 
 # Go to OTel Lambda Java folder
 cd ../opentelemetry-lambda/java || exit
-
+patch -p2 < "${SOURCEDIR}/../patches/opentelemetry-lambda_java.patch"
 ./gradlew build
 
 # Combine Java Agent build and ADOT Collector
@@ -41,6 +69,6 @@ mv otel-handler otel-handler-upstream
 mv otel-stream-handler otel-stream-handler-upstream
 mv otel-proxy-handler otel-proxy-handler-upstream
 cp "$SOURCEDIR"/scripts/* .
-unzip -qo ../../../../collector/build/collector-extension-$1.zip
+unzip -qo ${SOURCEDIR}/../opentelemetry-lambda/collector/build/collector-extension-$1.zip
 zip -qr opentelemetry-java-wrapper.zip *
 popd || exit
