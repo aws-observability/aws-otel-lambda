a2806660c72bd760694dc7195abe52e5c43fe615
Pavan Sai Vasireddy (Vasi) <pvasir@amazon.com>
2023-06-05 15:38:43 -0700

    Update Opentelemetry-lambda sub module

--- python/build.sh
+++ python/build.sh
@@ -24,8 +24,8 @@ cd ./build || exit
 pip install opentelemetry-sdk-extension-aws -t python/
 
 # Combine the layers
-unzip -qo layer.zip
-rm layer.zip
+unzip -qo opentelemetry-python-layer.zip
+rm opentelemetry-python-layer.zip
 unzip -qo ../../../collector/build/opentelemetry-collector-layer-$1.zip
 
 # Use our AWS scripts instead which extend and call OTel Lambda scripts
