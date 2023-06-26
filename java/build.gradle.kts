51e60b71e96dafe90aff646fda91dd2a683fcd5f
Raphael Silva <rapphil@gmail.com>
2023-06-26 20:42:31 +0000

    Revert "Force Java 8 only on the application"
    This reverts commit 9ec1564238ab02a68345dfb96e5dc733e482a95a.

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -12,16 +12,9 @@ repositories {
     mavenLocal()
 }
 
-allprojects {
-
-    plugins.apply("com.diffplug.spotless")
-
-    plugins.withId("java") { // Apply restriction on the project
-        configure<JavaPluginExtension> {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
-        }
-    }
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 spotless {
