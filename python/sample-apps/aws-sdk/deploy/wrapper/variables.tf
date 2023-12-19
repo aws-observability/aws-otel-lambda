fcb7712fb0e4083e6209f216877e8908e62b26e4
Vasi Vasireddy <pvasir@amazon.com>
2023-12-19 08:39:36 -0800

    Test Python 3.11 runtime in integration tests

--- python/sample-apps/aws-sdk/deploy/wrapper/variables.tf
+++ python/sample-apps/aws-sdk/deploy/wrapper/variables.tf
@@ -13,5 +13,5 @@ variable "architecture" {
 variable "runtime" {
   type        = string
   description = "Python runtime version used for sample Lambda Function"
-  default     = "python3.9"
+  default     = "python3.10"
 }
