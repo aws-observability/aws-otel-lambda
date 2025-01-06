f536de5d94083a68f560df70365afb6a9a49b511
Vasi Vasireddy <41936996+vasireddy99@users.noreply.github.com>
2025-01-06 11:19:37 -0800

    [chore] Fix sample app script files and arns used in the canaries (#1037)
    * fix sample app tf files and arns
    
    * Nodejs version 18 in workflow
    
    * Fix nodejs sample app  layer arns

--- nodejs/sample-apps/aws-sdk/deploy/wrapper/variables.tf
+++ nodejs/sample-apps/aws-sdk/deploy/wrapper/variables.tf
@@ -9,3 +9,9 @@ variable "architecture" {
   description = "Lambda function architecture, either arm64 or x86_64"
   default     = "x86_64"
 }
+
+variable "runtime" {
+  type        = string
+  description = "NodeJS runtime version used for sample Lambda Function"
+  default     = "nodejs18.x"
+}
