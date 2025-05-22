346830c798790a0b17476cca66b6d10b30686528
Vasi Vasireddy <pvasir@amazon.com>
2025-05-22 15:08:42 -0700

    Update Lambdasubmodule and remove old patches

--- patch-upstream.sh
+++ patch-upstream.sh
@@ -36,15 +36,11 @@ if [ -f $PATCH_OTEL_VERSION ]; then
     patch -p2 < $PATCH_OTEL_VERSION;
 fi
 
-# patch collector startup to add httpsprovider
+# patch collector startup to remove secretsmanagerprovider
 patch -p2 < ../../collector.patch
 
 # patch manager.go to remove lambdacomponents attribute
 patch -p2 < ../../manager.patch
-
-
-# patch listener.go to patch port usage
-patch -p2 < ../../listener.patch
 
 # Replace OTel Collector with ADOT Collector
 go mod edit -replace github.com/open-telemetry/opentelemetry-lambda/collector/lambdacomponents=${CURRENT_DIR}/adot/collector/lambdacomponents
