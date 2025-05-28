1dab4f9231d8657876bcb596be0745101bd42f0b
Vasi Vasireddy <pvasir@amazon.com>
2025-05-28 12:55:23 -0700

    Pass node instrumentations and modify build script

--- nodejs/build.sh
+++ nodejs/build.sh
@@ -22,9 +22,7 @@ mv ./packages/layer/build/workspace/otel-handler ./packages/layer/build/workspac
 cp "$SOURCEDIR"/scripts/otel-handler ./packages/layer/build/workspace/otel-handler
 # This assumes that generators and propagators does not have additional dependencies outside of normal OTel ones.
 # It's inconceivable for this to ever be incorrect.
-cp -r "$SOURCEDIR"/wrapper-adot/node_modules/@opentelemetry/id-generator-aws-xray ./packages/layer/build/workspace/nodejs/node_modules/@opentelemetry
-cp -r "$SOURCEDIR"/wrapper-adot/node_modules/@opentelemetry/propagator-aws-xray ./packages/layer/build/workspace/nodejs/node_modules/@opentelemetry
-cp -r "$SOURCEDIR"/wrapper-adot/node_modules/@opentelemetry/propagator-b3 ./packages/layer/build/workspace/nodejs/node_modules/@opentelemetry
+cp -r "$SOURCEDIR"/wrapper-adot/node_modules/@opentelemetry ./packages/layer/build/workspace/node_modules/
 cp "$SOURCEDIR"/wrapper-adot/build/src/adot-extension.* ./packages/layer/build/workspace/
 
 cd ./packages/layer/build/workspace || exit
